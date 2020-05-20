431faa06558075773e3e0c7f6cc3d00a20a810ca
nathan-stamand <nathansaint810@gmail.com>
2020-05-20 10:51:35 -0500

    added relationships between models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    belongs_to :author, 
+        :class_name => "User"
+    belongs_to :book
+    has_many :comments 
+    has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :user_books
+    has_many :books, through: :user_books
+    has_many :comments 
+    has_many :posts, through: :comments 
+    has_many :authored_posts,
+     :class_name => "Post", 
+     :foreign_key => :author_id
 end
