96c00765c96a88051ac305657c37192b24b9bf90
nathan-stamand <nathansaint810@gmail.com>
2020-05-21 11:04:40 -0500

    created new column in user table 'password_confirmation'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_150944) do
+ActiveRecord::Schema.define(version: 2020_05_21_160250) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_150944) do
     t.integer "pages_read"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_confirmation"
   end
 
 end
