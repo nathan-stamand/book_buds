aacd5ca9ea4cb8886c73be4f28a8aa9935b857e8
nathan-stamand <nathansaint810@gmail.com>
2020-05-25 12:55:19 -0500

    created #author_name=, #author_name, #genre_name=, and #genre_name methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,20 @@ class Book < ApplicationRecord
     has_many :posts
     belongs_to :author 
     belongs_to :genre
+
+    def author_name=(name)
+        self.author = Author.find_or_create_by(name: name)
+    end 
+
+    def author_name
+        self.author ? self.author.name : nil 
+    end
+
+    def genre_name=(name)
+        self.genre = Genre.find_or_create_by(name: name)
+    end
+
+    def genre_name 
+        self.genre ? self.genre.name : nil
+    end 
 end
