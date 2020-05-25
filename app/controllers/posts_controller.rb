50858dc2eac7e26c21cf2af572827a3e6c237d86
nathan-stamand <nathansaint810@gmail.com>
2020-05-25 15:29:37 -0500

    added CRUD methods and #post_params for secure params.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content, :author_id, :book_id)
+    end 
 end
