9bfe5be2379fabd9fc6aadb0eca316c676f655c7
nathan-stamand <nathansaint810@gmail.com>
2020-05-25 13:02:15 -0500

    moved #logged_in? and #current_user to ApplicationRecord model for wider use.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,2 @@
 module UsersHelper
-
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def current_user 
-        User.find_by(id: session[:user_id])
-    end 
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user 
+    User.find_by(id: session[:user_id])
+  end 
+
 end
