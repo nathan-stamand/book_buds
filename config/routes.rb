b431555e215d31da5164efac1e1e4aa9564108bc
nathan-stamand <nathansaint810@gmail.com>
2020-05-25 15:34:42 -0500

    added spacing and nested comments resource under Posts and posts resource under Books.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   resources :authors
   resources :genres
-  resources :comments
-  resources :posts
-  resources :books
+  
+  resources :posts do 
+    resources :comments 
+  end
+
+  resources :books do 
+    resources :posts 
+  end
   get '/users/login', to: 'users#login'
   post '/users/login', to: 'users#login'
   get '/users/logout', to: 'users#logout'
+  
   resources :users do 
     resources :books
     resources :posts
