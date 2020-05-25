fd3d4a93cabaac529a979cb7d6866c42e5d75bf1
nathan-stamand <nathansaint810@gmail.com>
2020-05-25 10:07:53 -0500

    added get/post users/login routes and get users/logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :posts
   resources :books
   get '/users/login', to: 'users#login'
+  post '/users/login', to: 'users#login'
+  get '/users/logout', to: 'users#logout'
   resources :users
   
 end
