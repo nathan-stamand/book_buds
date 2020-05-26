0d7edfdbc11b7c287d328cf7cf2d0cf688a1ebb4
nathan-stamand <nathansaint810@gmail.com>
2020-05-26 14:01:39 -0500

    added back :posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :authors, only: [:index, :show]
   resources :genres, only: [:index, :show]
+  resources :posts
 
   resources :books do 
     resources :posts 
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   get '/users/login', to: 'users#login'
   post '/users/login', to: 'users#login'
   get '/users/logout', to: 'users#logout'
+  post '/users/:id', to: 'users#show'
   
   resources :users do 
     resources :books
